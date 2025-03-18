cb70d7502d1415abb02d35c6467e3da7e772338d
henryle <le000379@umn.edu>
2025-03-17 19:52:17 -0500

    main.py connecting routes into main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from routes import router
+from database import engine
+from models import Base
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message" : "AI storyboard backend is running"}
+
+# Create the database tables
+Base.metadata.create_all(bind=engine)
+
+# Include routes
+app.include_router(router)
