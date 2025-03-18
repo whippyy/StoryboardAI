bcdb404ee259a89fafc97e4ff07b6640d6bacaad
henryle <le000379@umn.edu>
2025-03-17 20:01:42 -0500

    typo went unoticed

--- backend/models.py
+++ backend/models.py
@@ -4,6 +4,6 @@ from database import Base
 class Storyboard(Base):
     __tablename__ = "storyboards"
 
-    id = Column(Integer, primary_key = True, indes = True)
+    id = Column(Integer, primary_key = True, index = True)
     title = Column(String, index = True)
     descripton = Column(String)
