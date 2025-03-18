18f25794584c138f4636733f4a60b046b2bb62fd
henryle <le000379@umn.edu>
2025-03-17 19:56:31 -0500

    some issues with starting uvicorn

--- backend/database.py
+++ backend/database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgressql://user:password@localhost/storyboard_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/storyboard_db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
